1f6245116e7177b6c981cddf982da72999cd3653
ManausK <1905246@kiit.ac.in>
2023-04-27 14:31:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const image = document.querySelector(".container");
+heartIcon = document.querySelector(".heart");
+
+image.addEventListener("dblclick", (e) => {
+  let xValue = e.clientX - e.target.offsetLeft;
+  let yValue = e.clientY - e.target.offsetTop;
+  console.log(xValue, yValue);
+
+  heartIcon.style.left = `${xValue}px`;
+  heartIcon.style.top = `${yValue}px`;
+
+  heartIcon.classList.add("active");
+
+  setTimeout(() => {
+    heartIcon.classList.remove("active");
+  }, 1000);
+});
